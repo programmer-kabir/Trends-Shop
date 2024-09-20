598fe4ee0925db0811923389a8ebde385e5e16e1
Your Name <you@example.com>
2024-09-20 11:20:22 +0600

    user data send backend

--- Client/src/Pages/Authenction/Register/Register.jsx
+++ Client/src/Pages/Authenction/Register/Register.jsx
@@ -15,10 +15,24 @@ const Register = () => {
       updateUserProfile(values.name)
       .then((data) => {
         
-        // console.log(user);
-      });
+        const currentUser = {
+          name:user?.displayName,
+          email:user?.email,
+      }
+      fetch(`${import.meta.env.VITE_LOCALHOST_KEY}/users`,{
+        method:'POST',
+        headers:{
+            'content-type':'application/json'
+        },
+        body:JSON.stringify(currentUser)
+    })
+    .then(res => res.json())
+    .then(data =>{
+      console.log(data);
       toast.success("Registration successful! You can now log in.");
       navigate('/')
+    })
+      });
     })
     .catch((error) => {
         toast.error(error.message);
--- Server/app.js
+++ Server/app.js
@@ -4,14 +4,12 @@ const port = 3000;
 const cors = require("cors");
 require("dotenv").config();
 
-
 //Middleware
 app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0i3pjbq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0i3pjbq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,14 +25,31 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const shoesCollection = client.db("TrendsShop").collection("shoes");
+    const usersCollection = client.db("TrendsShop").collection("users");
 
     // shoes get
-    app.get('/shoes', async(req, res) =>{
-      const result = await shoesCollection.find().toArray()
-      res.send(result)
-    })
+    app.get("/shoes", async (req, res) => {
+      const result = await shoesCollection.find().toArray();
+      res.send(result);
+    });
 
-
+    // User
+    // Post all User
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exist" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    // Get all user
+    app.get("/users", async (req, res) => {
+      const user = await usersCollection.find().toArray();
+      res.send(user);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("You successfully connected to MongoDB!");
